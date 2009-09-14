e61bf0be6fe8753ceb3ebd916c34bfd21844aed2
Lindsay Evans <linz@linz.id.au>
2009-09-14 19:23:38 +1000

    Adding redirect following

--- doit.rb
+++ doit.rb
@@ -22,34 +22,60 @@ require 'yaml'
 start_date = DateTime.now
 results_filename = "results-#{start_date.strftime("%Y%m")}"
 
-Ccsv.foreach(top_sites_file) do |values|
+
+def fetch(uri, limit = 10)
+
+    uri = URI.parse uri
+
+    if limit == 0
+	{:uri => uri.to_s, :message => "Reached redirect limit"}
+    end
 
     begin
-	domain = values[1]
 
-	puts "checking #{values[0]} #{domain}"
-
-	uri = URI.parse "http://#{domain}/"
 	response = Net::HTTP.get_response uri
 
-	fd = File.open(results_filename, "a")
-	fd.write YAML::dump({
-	    :domain => domain,
+	ret = {
+	    :uri => uri.to_s,
 	    :code => response.code,
 	    :headers => response.header.to_hash, 
 	    :http_version => response.http_version,
 	    :message => response.message
-	})
-	#fd.write YAML::dump(response)
+	}
+
+	case response
+	when Net::HTTPSuccess     then ret
+	when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+	end
+
 	rescue => e
 	    puts 'Exception:'
 	    puts YAML::dump(e)
+	    {:uri => uri.to_s, :message => "Exception: #{e.message}"}
+	    
 	rescue Timeout::Error => e
 	    puts 'Timeout error:'
 	    puts YAML::dump(e)
+	    {:uri => uri.to_s, :message => "Timeout error: #{e.message}"}
 
     end
-    #break if values[0].to_i > 1
+
+end
+
+
+Ccsv.foreach(top_sites_file) do |values|
+	domain = values[1]
+
+	puts "checking #{values[0]} #{domain}"
+
+	uri = "http://#{domain}/"
+	response = fetch(uri)
+	response[:domain] = domain
+#puts YAML::dump(response)
+	fd = File.open(results_filename, "a")
+	fd.write YAML::dump(response)
+	fd.close
+    break if values[0].to_i > 1
 
 end
 
