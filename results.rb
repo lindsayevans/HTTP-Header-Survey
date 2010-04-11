6f46e50bcf6cca26a0789d6fb955b0f1a89a54ea
Lindsay Evans <linz@linz.id.au>
2010-04-11 11:35:19 +1000

    Doing CSV conversion

--- results.rb
+++ results.rb
@@ -1,4 +1,7 @@
 require 'yaml'
+require 'rubygems'
+require 'faster_csv'
+require 'activesupport'
 
 @counts = {}
 
@@ -18,33 +21,70 @@ def count_header ydoc, sym
     end
 end
 
-def print_results f
 
-File.open(f) do |yf|
-    YAML.load_documents(yf) do |ydoc|
-	count_occurrences ydoc, :redirects
-	count_occurrences ydoc, :code
-	count_occurrences ydoc, :message
-	count_occurrences ydoc, :use_ssl
-	count_occurrences ydoc, :http_version
-	count_occurrences ydoc, :port
-
-	if ydoc.has_key? :headers
-	    count_header ydoc[:headers], 'x-powered-by'
-	    #count_header ydoc[:headers], 'server'
-	end
+def count_server_header ydoc, sym
+    if ydoc.has_key? sym
+	v = ydoc[sym][0].split('/').first
+	@counts[sym] = {} if !@counts.has_key? sym
+	@counts[sym][v] = @counts[sym].has_key?(v) ? @counts[sym][v] + 1 : 1
     end
 end
 
-puts "*" * 80
-puts "File: #{f}"
-puts "Counts:"
-puts YAML::dump(@counts)
+def print_results f
+
+  props = ['message', 'code', 'domain', 'redirects', 'http_version', 'port', 'host', 'use_ssl', 'path','headers']
+  headers = []
+  csv = []
+
+  File.open(f) do |yf|
+    YAML.load_documents(yf) do |ydoc|
+	#props.concat(ydoc.stringify_keys.keys)
+	headers.concat(ydoc[:headers].keys) if ydoc.has_key? :headers
+    end
+
+    puts "*" * 80
+    puts "Input file: #{f}"
+    #puts props.uniq.concat(headers.uniq.sort).join(',')
+
+    headers = headers.uniq.sort
+    csv << props.uniq.concat(headers)
+
+  end
+
+  File.open(f) do |yf|
+
+    YAML.load_documents(yf) do |ydoc|
+
+      data = [
+	ydoc[:message],	ydoc[:code], ydoc[:domain], ydoc[:redirects], ydoc[:http_version], ydoc[:port], ydoc[:host], ydoc[:use_ssl], ydoc[:path], ydoc.has_key?(:headers) ? ydoc[:headers].length : 0
+      ]
+
+      headers.each do |h|
+	if ydoc.has_key?(:headers) && ydoc[:headers].has_key?(h)
+	  data.push(ydoc[:headers][h])
+	else
+	  data.push(nil)
+	end
+      end
+
+      #puts data.join(',')
+
+      csv << data
+    end
+
+  end
+
+  FasterCSV.open("#{f.gsub('results/','results/csv/')}.csv", "w") do |c|
+    csv.each do |r|
+      c << r
+    end
+  end
+
+  puts "Output file: #{f.gsub('results/','results/csv/')}.csv"
 
 end
 
-print_results 'results/results-200911'
-@counts = {}
-
-print_results 'results/results-200912'
-
+Dir.glob("results/results-*").each do |f|
+  print_results f
+  @counts = {}
+end
